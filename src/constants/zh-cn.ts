6771190dd4e05410b6826333c7e6c903eb0c35b7
yangyao <873241789@qq.com>
2019-02-21 17:40:54 +0800

    新增 menu 常量定义

---
+++ src/constants/zh-cn.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019/2/21
+ */
+
+//  菜单
+export const menu = {
+  dashboard: '仪表盘',
+  tradeQuery: '交易查询'
+};
