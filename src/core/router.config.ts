dfb7faa8fd27b1f4962b2049dca35fcbcb045ace
yangyao <873241789@qq.com>
2019-02-21 17:36:11 +0800

    完善路由  新增仪表盘  交易查询路由 分离路由path

--- src/core/router.config.ts
+++ src/core/router.config.ts
@@ -1,6 +1,20 @@
-import { ApplicationIndex } from '../container/Application';
+import { Dashboard } from '../container/Dashboard';
+import { TradeQuery } from '../container/Business/TradeQuery';
+
+import { menu } from '../constants/zh-cn';
+
+// 路由路径
+export const routerPath = {
+  tradeQuery: '/business/tradeQuery',
+  dashboard: '/dashboard'
+};
 
 export default [{
-  path: '/home',
-  component: ApplicationIndex,
+  path: routerPath.dashboard,
+  title: menu.dashboard,
+  component: Dashboard
+}, {
+  path: routerPath.tradeQuery,
+  title: menu.tradeQuery,
+  component: TradeQuery
 }];
