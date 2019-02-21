42096b1f91ee37fb7621adbe0a2c01117c388ce9
yangyao <873241789@qq.com>
2019-02-21 17:40:22 +0800

    完善 交易查询模块

---
+++ src/container/Business/TradeQuery.tsx
@@ -0,0 +1,15 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019/2/21
+ */
+import * as React from 'react';
+
+export class TradeQuery extends React.PureComponent<any, any> {
+  render() {
+    return (
+      <div>
+        {'tradeQuery'}
+      </div>)
+  }
+}
