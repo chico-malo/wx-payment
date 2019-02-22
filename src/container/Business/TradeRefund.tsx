fbee6081eea71a8b5f6c0ce9ab34f5f5ba1ee11f
yangyao <873241789@qq.com>
2019-02-22 18:13:54 +0800

    新增 交易退款模块

---
+++ src/container/Business/TradeRefund.tsx
@@ -0,0 +1,16 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019/2/22
+ */
+import * as React from 'react';
+
+export class TradeRefund extends React.PureComponent<any, any> {
+  render() {
+    return (
+      <div>
+        {'TradeRefund'}
+      </div>
+    )
+  }
+}
