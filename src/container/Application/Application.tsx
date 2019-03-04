c16ac15b317e186dbe27ff86d0d9ff754679ec6a
yangyao <873242789@qq.com>
2019-03-04 15:14:19 +0800

    分离 application 容器

---
+++ src/container/Application/Application.tsx
@@ -0,0 +1,58 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019-03-04
+ */
+import * as React from 'react';
+import { autoBind } from '@sitb/wbs/autoBind';
+import { Header } from './Header';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles } from '../../utils/withStyles';
+import { Menu } from './Menu';
+import { Content } from './Content';
+
+const styles: any = theme => ({
+  root: {
+    width: '100%',
+    display: 'flex'
+  }
+});
+
+@withStyles(styles)
+@autoBind
+export class Application extends React.PureComponent<any, any> {
+  state = {
+    menuOpen: true
+  };
+
+  /**
+   * 打开菜单
+   */
+  handleMenuOpen() {
+    this.setState({menuOpen: true});
+  };
+
+  /**
+   * 缩小菜单
+   */
+  handleMenuClose() {
+    this.setState({menuOpen: false});
+  };
+
+  render() {
+    const {classes} = this.props;
+    const {menuOpen} = this.state;
+    return (
+      <div className={classes.root}>
+        <CssBaseline/>
+        <Header menuOpen={menuOpen}
+                handleMenuOpen={this.handleMenuOpen}
+        />
+        <Menu menuOpen={menuOpen}
+              handleMenuClose={this.handleMenuClose}
+        />
+        <Content menuOpen={menuOpen}/>
+      </div>
+    )
+  }
+}
