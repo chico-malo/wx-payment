441b69157d7b09676e7c20428ab87f178026d8df
yangyao <873242789@qq.com>
2019-03-04 10:07:20 +0800

    规范接口，完善清除 提交函数

--- src/component/Form/index.tsx
+++ src/component/Form/index.tsx
@@ -4,11 +4,11 @@
  * date: 2019-02-26
  */
 import * as React from 'react';
-import { Button, Paper } from '@material-ui/core';
+import { Button, Grid, Paper } from '@material-ui/core';
 import Form from 'veigar/Form';
 import { autoBind } from '@sitb/wbs/autoBind';
 import { withStyles } from '@sitb/wbs/mui/withStyles';
-import { FieldProps, Field, variantProps } from './Field';
+import { Field, FieldProps, variantProps } from './Field';
 
 export interface FieldGroup {
   fieldGroup: Array<FieldProps>
@@ -17,11 +17,23 @@ export interface FieldGroup {
 
 export interface FormProps {
   classes?: any;
+  /**
+   * 表单组配置
+   */
   fieldGroups: Array<FieldGroup>;
-  unifiedVariant: variantProps
+  /**
+   * 表单layout
+   */
+  unifiedVariant: variantProps;
+  onSubmit: (values) => void;
+  onReset: () => void;
 }
 
 const styles = theme => ({
+  paper: {
+    marginTop: 24,
+    padding: 12
+  },
   button: {
     margin: theme.spacing.unit
   }
@@ -36,23 +48,39 @@ export class FormContainer extends React.PureComponent<FormProps, any> {
     unifiedVariant: 'outlined'
   };
 
-  handlePasswordChange() {
+  /**
+   * 提交
+   */
+  onSubmit() {
     const errorFields = this.form.validate();
     if (errorFields) {
       console.log(errorFields);
       return;
     }
+    const {onSubmit} = this.props;
     const values = this.form.getValue();
-    console.log(values);
+    onSubmit && onSubmit(values);
+  }
+
+  /**
+   * 重置
+   */
+  onReset() {
+    const {onReset} = this.props;
+    this.form.onReset();
+    onReset && onReset();
   }
 
   render() {
     const {classes, fieldGroups, unifiedVariant} = this.props;
     return (
-      <Paper>
+      <Paper className={classes.paper}>
         <Form ref={node => this.form = node}>
           {fieldGroups.map(({fieldGroup, title}, index) => (
-            <div key={index} className="form-group">
+            <Grid key={index}
+                  container
+                  spacing={24}
+            >
               <h1>{title}</h1>
               {fieldGroup.map(({...field}: any, idx) => (
                 <Field variant={unifiedVariant}
@@ -60,19 +88,26 @@ export class FormContainer extends React.PureComponent<FormProps, any> {
                        {...field}
                 />
               ))}
-            </div>
+            </Grid>
           ))}
         </Form>
-        <Button variant="contained" className={classes.button}>
-          {'清空'}
-        </Button>
-        <Button variant="contained"
-                color="primary"
-                className={classes.button}
-                onClick={this.handlePasswordChange}
+        <Grid container
+              justify="flex-end"
         >
-          {'搜索'}
-        </Button>
+          <Button variant="contained"
+                  className={classes.button}
+                  onClick={this.onReset}
+          >
+            {'清空'}
+          </Button>
+          <Button variant="contained"
+                  color="primary"
+                  className={classes.button}
+                  onClick={this.onSubmit}
+          >
+            {'搜索'}
+          </Button>
+        </Grid>
       </Paper>
     );
   }
