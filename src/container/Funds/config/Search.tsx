1d0a88720fac362c83c27f434df4b306bc03d75a
yangyao <873242789@qq.com>
2019-03-05 18:51:57 +0800

    配置 结算账户 search表单

--- src/container/Funds/config/Search.tsx
+++ src/container/Funds/config/Search.tsx
@@ -6,11 +6,15 @@
 import { FieldGroupItemProps } from '../../../component/Form/FieldGroup';
 import { lang } from '../../../constants/zh-cn';
 
+// 结算模块 搜索表单
 export const settleSearch: Array<FieldGroupItemProps> = [{
   fieldGroup: [{
     fields: [{
       label: lang.merchantNo,
-      name: 'merchantNo'
+      name: 'merchantNo',
+      required: true,
+      missText: '商户号不能为空',
+      type: 'select'
     }, {
       label: lang.fundsSettlement.startAmount,
       name: 'startAmount'
