e0089200806e9adc9f0c42e21ec9a030799eb4e5
yangyao <873242789@qq.com>
2019-03-07 18:27:35 +0800

    新增 结算信息状态options

---
+++ src/constants/select/settleStatus.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019-03-07
+ */
+import { lang } from '../zh-cn';
+
+// 结算信息 状态
+export const settleStatus = {
+  UNTREATED: lang.untreated,
+  SUCCESS: lang.success,
+  FAILURE: lang.failure,
+  PROCESSING: lang.processing,
+  EXCEPTION: lang.exception
+};
