dcc7d4efb6b0295680deab4b167b833806ba33a2
yangyao <873242789@qq.com>
2019-03-07 18:29:32 +0800

    新增 moment 时间转换

--- src/container/Funds/config/Columns.ts
+++ src/container/Funds/config/Columns.ts
@@ -5,6 +5,9 @@
  */
 import { ColumnsItem } from '../../../component/Table/EnhancedTableHead';
 import { lang } from '../../../constants/zh-cn';
+import { settleStatus } from '../../../constants/select/settleStatus';
+import { SettleCycleOptions } from '../../../constants/select/settleCycle';
+import moment from 'moment';
 
 export const settleColumns: Array<ColumnsItem> = [{
   id: 'auditNumber',
@@ -17,22 +20,27 @@ export const settleColumns: Array<ColumnsItem> = [{
   label: lang.fundsSettlement.realSettleAmount
 }, {
   id: 'status',
-  label: lang.fundsSettlement.status
+  label: lang.fundsSettlement.status,
+  render: status => status && settleStatus[status]
 }, {
   id: 'settleCycle',
-  label: lang.fundsSettlement.settleCycle
+  label: lang.fundsSettlement.settleCycle,
+  render: settleCycle => settleCycle && SettleCycleOptions[settleCycle]
 }, {
   id: 'totalAmount',
   label: lang.fundsSettlement.totalAmount
 }, {
   id: 'settleAt',
-  label: lang.fundsSettlement.settleAt
+  label: lang.fundsSettlement.settleAt,
+  render: (settleAt) => settleAt && moment(settleAt).format('YYYY-MM-DD')
 }, {
   id: 'createAt',
-  label: lang.createAt
+  label: lang.createAt,
+  render: (createAt) => createAt && moment(createAt).format('YYYY-MM-DD')
 }, {
-  id: 'update',
-  label: lang.updateAt
+  id: 'updateAt',
+  label: lang.updateAt,
+  render: (updateAt) => updateAt && moment(updateAt).format('YYYY-MM-DD')
 }, {
   id: 'remark',
   label: lang.remark
