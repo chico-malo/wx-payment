2bd08bda3d6cc916c64c71f9698dbe361c124979
yangyao <873242789@qq.com>
2019-03-07 18:28:01 +0800

    新增 结算类型options

---
+++ src/constants/select/settleCycle.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019-03-07
+ */
+
+import { lang } from '../zh-cn';
+
+/**
+ * 结算周期
+ */
+export const SettleCycleOptions = {
+  "D0": lang.d0,
+  "T0": lang.t0,
+  "T1": lang.t1,
+  "T7": lang.t7,
+  "T15": lang.t15,
+  "T30": lang.t30,
+  "T90": lang.t90,
+  "T180": lang.t180,
+  "T135": lang.t365,
+  "manual": lang.manual
+};
