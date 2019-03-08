307d0723f1f9f916346d2eefd219d994c6f0dc93
yangyao <873242789@qq.com>
2019-03-08 15:16:09 +0800

    add trade businessType status options

---
+++ src/constants/select/businessType.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019-03-08
+ */
+
+let businessType = require('../businessType.json');
+
+export const businessTypeOptions = businessType;
---
+++ src/constants/select/tradeStatus.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019-03-08
+ */
+import { lang } from '../zh-cn';
+
+export const tradeStatus = {
+  /**
+   * 成功
+   */
+  SUCCESS: lang.success,
+
+  /**
+   * 失败
+   */
+  FAILURE: lang.failure,
+
+  /**
+   * 待支付
+   */
+  WAIT_PAYMENT: lang.waitPayment,
+
+  /**
+   * 处理中
+   */
+  PROCESSING: lang.processing,
+
+  /**
+   * 交易关闭
+   */
+  CLOSE: lang.trade.close,
+
+  /**
+   * 删除
+   */
+  DELETE: lang.trade.delete
+
+};
